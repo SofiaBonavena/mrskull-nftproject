9edf2bc32c20a9d284956476be3be88b64d03ff5
Leandro Amaro Garcia <leanamaro@gmail.com>
2022-11-10 21:23:47 -0300

    added mint module

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
-import React from "react";
-import WalletButton from "../components/WalletButton/WalletButton";
-import WalletClient from "../components/WalletClient/WalletClient";
+import React from 'react';
+import Mint from '../components/Mint/Mint';
+import WalletClient from '../components/WalletClient/WalletClient';
 
 const Home = () => {
   return (
     <>
-      <WalletButton />
-      <WalletClient />
+      <div className='homeContainer'>
+        <h1>Minimal Mint</h1>
+        <div className='modulesContainer'>
+          <WalletClient />
+          <Mint />
+        </div>
+      </div>
     </>
   );
 };
